a323a9cb1745c3a721f6fb76c3ece3a46ee4ffbb
ArianMilftari <anonymus0828491@gmail.com>
2026-07-21 20:21:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -190,3 +190,19 @@ window.addEventListener("load",()=>{
     document.body.style.opacity="1";
 
 });
+// ==========================
+// PREMIUM SPOTLIGHT
+// ==========================
+
+cards.forEach(card => {
+
+    card.addEventListener("mousemove", (e) => {
+
+        const rect = card.getBoundingClientRect();
+
+        card.style.setProperty("--x", (e.clientX - rect.left) + "px");
+        card.style.setProperty("--y", (e.clientY - rect.top) + "px");
+
+    });
+
+});
